0921b3fe516f47729da71d4f41d64bc16830ce6a
chatomzcoding <coding.chatomz@gmail.com>
2022-12-07 14:58:41 +0700

    index maja

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MajaController::class,'index']);
 
 Route::prefix('dev')->group(function () {
     Route::get('/inquiry', [MajaController::class,'inquiry']);
